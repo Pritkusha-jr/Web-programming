d29f89dcae21e32755a98da5160a26d647b383b9
Sofiya Prytkova <prytkova.sofiya@gmail.com>
2023-05-22 22:55:39 +0700

    Недоделано количество дней с др

--- lab8.js
+++ lab8.js
@@ -7,3 +7,12 @@ function showDate() {
     out.innerHTML += 'Дата и время для локали Японии: ' + today.toLocaleString('ja-JP') + '<br>';
     out.innerHTML += 'Дата и время для локали Германии: ' + today.toLocaleString('de') + '<br>';
 }
+
+function showDayCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+}
+
