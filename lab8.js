a9a619eea57fcb388991f953ac329714ffd4304b
Sofiya Prytkova <prytkova.sofiya@gmail.com>
2023-05-24 10:35:37 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -13,7 +13,9 @@ function showDayCount() {
     let inputDate = document.querySelector('input[type=date]');
     let birthday = new Date(inputDate.value);
     let daysCount = (today - birthday)/1000/60/60/24;
+    let div = document.getElementById('div1');
     daysCount = Math.floor(daysCount);
+    div.innerHTML = "Количество дней с даты рождения: " + daysCount
 }
 
 function showTime() {
